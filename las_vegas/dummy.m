cc6c6200ee02cef464ec90a6b685ad8407835f9c
Rishabh Panda <80598737+rishabh-panda@users.noreply.github.com>
2021-12-29 15:37:40 +0530

    Delete las_vegas directory

--- las_vegas/dummy.m
+++
@@ -1,8 +0,0 @@
-IDX_ok_1 = find(allvarf<1.0);
-IDX_ok_2 = find(allvarf<2.0);
-IDX_ok_3 = find(allvarf<3.0);
-IDX_ok_4 = find(allvarf<4.0);
-allESTPS_1= allESTPS(:,IDX_ok_1);
-allESTPS_2= allESTPS(:,IDX_ok_2);
-allESTPS_3= allESTPS(:,IDX_ok_3);
-allESTPS_4= allESTPS(:,IDX_ok_4);
--- las_vegas/testcoh.m
+++
@@ -1,5 +0,0 @@
-coh = zeros(5000*200,1);
-for i = 1:5000*200
-    coh(i) = double(enscoh(phi(i,:)));
-    ml(i) = gamma_ml(phi(i,:));
-end
